8392bc431169d735d6a9800962c5449b49efc32f
sadigirlmd-debug <sadigirlmd@gmail.com>
2025-09-23 23:31:07 +0530

    Update s.js

--- plugins/s.js
+++ plugins/s.js
@@ -57,7 +57,7 @@ cmd(
     async (conn, mek, m, { quoted, args, q, reply, from }) => {
         if (!mek.quoted) return reply(`*Reply to any Image or Video, Sir.*`);
         let mime = mek.quoted.mtype;
-        let pack = Config.STICKER_NAME || "Rukshan lod";
+        let pack = 🧙‍♂️ 𝐙𝐀𝐍𝐓𝐀 × 𝐌𝐃 𝐎𝐅𝐂 🧙‍♂️";
         
         if (mime === "imageMessage" || mime === "stickerMessage") {
             let media = await mek.quoted.download();
