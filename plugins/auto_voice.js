c87ed3702814f69a29077240ee1720f7f66aa40c
sadigirlmd-debug <sadigirlmd@gmail.com>
2025-09-23 23:53:33 +0530

    Update auto_voice.js

--- plugins/auto_voice.js
+++ plugins/auto_voice.js
@@ -4,7 +4,7 @@ const { cmd } = require("../command");
 
 cmd({ on: "body" }, async (conn, m, msg, { from, body }) => {
   try {
-    const jsonUrl = "https://files.catbox.moe/4rt5h1";
+    const jsonUrl = "https://raw.githubusercontent.com/JawadYT36/KHAN-DATA/main/autovoice.json";
     const res = await axios.get(jsonUrl);
     const voiceMap = res.data;
 
