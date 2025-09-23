9a87d5fde8a29f5c430c594e61621675a4869ef8
sadigirlmd-debug <sadigirlmd@gmail.com>
2025-09-23 11:58:06 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -7,17 +7,15 @@ cmd({
     alias: ["zanta","suranga"],
     desc: "Check up time , ram usage and more",
     category: "main",
-    react: "💻",
+    react: "👤",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let status = `╭━━〔 *🧙‍♂️ 𝐙𝐀𝐍𝐓𝐀 × 𝐌𝐃 𝐎𝐅𝐂 🧙‍♂️* 〕━━┈⊷
 ┃◈╭─────────────·๏
-┃◈┃• *⏳Uptime*: ${runtime(process.uptime())}
-┃◈┃• *📟 Ram*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
-┃◈┃• *⚙️ Platform:-* ${os.hostname()}
-┃◈┃• *👨‍💻 Owner*: ᴍʀ ꜱᴜʀᴀɴɢᴀ ᴍᴏᴅ-ᴢ🧸
+┃◈┃• *👤 number :-* 94760264995
+┃◈┃• *👨‍💻 Owner* : ᴍʀ ꜱᴜʀᴀɴɢᴀ ᴍᴏᴅ-ᴢ🧸
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
 `
