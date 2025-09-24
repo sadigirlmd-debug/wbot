d8756b63fc93e0bb986b1e65ad8e607812869b38
sadigirlmd-debug <sadigirlmd@gmail.com>
2025-09-24 08:36:08 +0530

    Update jock.js

--- plugins/jock.js
+++ plugins/jock.js
@@ -18,7 +18,7 @@ cmd({
       return reply("❌ Failed to fetch a joke. Please try again.");
     }
 
-    const jokeMessage = `🤣 *Here's a random joke for you!* 🤣\n\n*${joke.setup}*\n\n${joke.punchline} 😆\n\n> *© Powered by JawadTechX*`;
+    const jokeMessage = `🤣 *Here's a random joke for you!* 🤣\n\n*${joke.setup}*\n\n${joke.punchline} 😆\n\n> *🧙‍♂️ 𝐙𝐀𝐍𝐓𝐀 × 𝐌𝐃 𝐎𝐅𝐂 🧙‍♂️*`;
 
     return reply(jokeMessage);
   } catch (error) {
